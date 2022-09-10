5690c5c7a1a5ab73198049f47afcab53a913ddb1
Chris Seaman <christopherseaman1983@gmail.com>
2022-09-10 12:32:07 +0100

    new test.js added to rewrite codefor learning

--- versiontwo.js
+++ versiontwo.js
@@ -1,10 +1,11 @@
 const imgContainer = document.getElementById("img-container");
+imgContainer.addEventListener("click", handleDuckClicks);
 const img1 = document.getElementById("img1");
 const img2 = document.getElementById("img2");
 const img3 = document.getElementById("img3");
 
 let clicks = 0;
-let maxClicksAllowed = 8;
+const maxClicksAllowed = 10;
 
 function OddDuck(name, src) {
   this.name = name;
@@ -16,13 +17,12 @@ function OddDuck(name, src) {
 }
 
 OddDuck.allDucksArray = [];
-let usedDucks = [];
 
 function getRandomIndex() {
   return Math.floor(Math.random() * OddDuck.allDucksArray.length);
 }
 
-function render() {
+function renderDucks() {
   let oddDuck1 = getRandomIndex();
   let oddDuck2 = getRandomIndex();
   let oddDuck3 = getRandomIndex();
@@ -30,10 +30,7 @@ function render() {
   while (
     oddDuck1 === oddDuck2 ||
     oddDuck1 === oddDuck3 ||
-    oddDuck2 === oddDuck3 ||
-    usedDucks.includes(oddDuck1) ||
-    usedDucks.includes(oddDuck2) ||
-    usedDucks.includes(oddDuck3)
+    oddDuck2 === oddDuck3
   ) {
     let oddDuck1 = getRandomIndex();
     let oddDuck2 = getRandomIndex();
@@ -43,6 +40,7 @@ function render() {
   img1.src = OddDuck.allDucksArray[oddDuck1].src;
   img2.src = OddDuck.allDucksArray[oddDuck2].src;
   img3.src = OddDuck.allDucksArray[oddDuck3].src;
+
   img1.alt = OddDuck.allDucksArray[oddDuck1].name;
   img2.alt = OddDuck.allDucksArray[oddDuck2].name;
   img3.alt = OddDuck.allDucksArray[oddDuck3].name;
@@ -50,34 +48,29 @@ function render() {
   OddDuck.allDucksArray[oddDuck1].views++;
   OddDuck.allDucksArray[oddDuck1].views++;
   OddDuck.allDucksArray[oddDuck1].views++;
-
-  usedDucks = [];
-  usedDucks.push(oddDuck1, oddDuck2, oddDuck3);
 }
 
 function handleDuckClicks(event) {
   if (event.target === imgContainer) {
-    alert("Please click on an Image.");
+    alert("Please click on an image.");
   } else {
     clicks++;
-    let clickDuck = event.target.alt;
+    let currentClick = event.target.alt;
 
     for (let i = 0; i < OddDuck.allDucksArray.length; i++) {
-      if (clickDuck === OddDuck.allDucksArray[i].name) {
+      if (currentClick === OddDuck.allDucksArray[i].name) {
         OddDuck.allDucksArray[i].clicks++;
         break;
       }
     }
 
-    if (clicks === maxClicksAllowed) {
-      imgContainer.removeEventListener("click", handleDuckClicks);
-
+    if (maxClicksAllowed === clicks) {
       alert(
-        "Thank you so much, now click view results to see which products you voted for."
+        "Thank you, click view results to se which products are the most popular."
       );
-    } else {
-      render();
     }
+
+    renderDucks();
   }
 }
 
@@ -87,18 +80,18 @@ new OddDuck("bathroom", "./images/bathroom3x.jpg");
 new OddDuck("boots", "./images/boots3x.jpg");
 new OddDuck("breakfast", "./images/breakfast3x.jpg");
 new OddDuck("bubblegum", "./images/bubblegum3x.jpg");
-new OddDuck("chair3x", "./images/cthulhu3x.jpg");
-new OddDuck("dog duck", "./images/dog-duck@3x.jpg");
+new OddDuck("chair", "./images/chair3x.jpg");
+new OddDuck("cthulhu", "./images/cthulhu3x.jpg");
+new OddDuck("dog-duck", "./images/dog-duck@3x.jpg");
 new OddDuck("dragon", "./images/dragon3x.jpg");
 new OddDuck("pen", "./images/pen3x.jpg");
-new OddDuck("pet sweep", "./images/pet-sweep3x.jpg");
+new OddDuck("pet-sweep", "./images/pet-sweep3x.jpg");
 new OddDuck("scissors", "./images/scissors3x.jpg");
 new OddDuck("shark", "./images/shark3x.jpg");
 new OddDuck("sweep", "./images/sweep3x.jpg");
 new OddDuck("tauntaun", "./images/tauntaun3x.jpg");
 new OddDuck("unicorn", "./images/unicorn3x.jpg");
-new OddDuck("water can", "./images/water-can3x.jpg");
-new OddDuck("wine glass", "./images/wine-glass3x.jpg");
+new OddDuck("water-can", "./images/water-can3x.jpg");
+new OddDuck("wine-glass", "./images/wine-glass3x.jpg");
 
-imgContainer.addEventListener("click", handleDuckClicks);
-render();
+renderDucks();
